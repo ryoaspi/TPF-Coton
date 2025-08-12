48291682380ea38bdc67495f02d6382ed2c99331
formatechnomash <formatechnocite@gmail.com>
2025-08-12 13:48:40 +0200

    Camera script added
    not finished yet

---
+++ TPF-Coton/Assets/_/Features/Camera/Runtime/CameraRelativeMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Camera.Runtime
+{
+    public class CameraRelativeMovement : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
