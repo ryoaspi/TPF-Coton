e57f46f930075d414cb70b53823499c7000bb4b6
Thomas <>
2025-09-02 14:34:05 +0200

    Adaptation LoseCoton and Value Coton

--- TPF-Coton/Assets/_/Features/Object/Runtime/Coton.cs
+++ TPF-Coton/Assets/_/Features/Object/Runtime/Coton.cs
@@ -33,7 +33,6 @@ namespace Object.Runtime
                 _col.enabled = false;
                 _col.enabled = true;
                 
-                Debug.Log("Coton collectable");
             }
         }
         
@@ -51,7 +50,6 @@ namespace Object.Runtime
         private void OntriggerEnter(Collider other)
         {
             if (!CanBeCollected())return;
-            Debug.Log($"Coton touché par : {other.gameObject.name}");
         }
 
         #endregion
@@ -61,7 +59,7 @@ namespace Object.Runtime
         public int Collect()
         {
             _collected = true;
-            return 1;
+            return _valueCoton;
         }
 
         public void SetPhysicsActive(bool active)
@@ -73,10 +71,14 @@ namespace Object.Runtime
         public bool CanBeCollected()
         {
             bool canCollect = Time.time >= _spawnTime + _collectibleDelay;
-            Debug.Log($"Can be collected : {canCollect}");
             return canCollect;
         }
-        
+
+        public int GetCotonValue(int damage)
+        {
+            _valueCoton = damage;
+            return _valueCoton;
+        }
         
         #endregion
         
@@ -102,6 +104,8 @@ namespace Object.Runtime
         [SerializeField] private float _collectibleDelay = 0.5f;
 
         private bool _wasCollectable;
+        private int _valueCoton = 1;
+
 
         #endregion
     }
--- TPF-Coton/Assets/_/Features/Player/Runtime/PlayerBuff.cs
+++ TPF-Coton/Assets/_/Features/Player/Runtime/PlayerBuff.cs
@@ -8,8 +8,7 @@ namespace Player.Runtime
     public class PlayerBuff : MonoBehaviour
     {
         #region Publics
-
-        [HideInInspector] public int m_buff;
+        
         [HideInInspector] public int m_coton;
 
         #endregion
@@ -20,7 +19,6 @@ namespace Player.Runtime
         private void Start()
         {
             _playerStats=GetComponent<PlayerStats>();
-            _playerMovement=GetComponent<PlayerMovement>();
             m_coton = _playerStats.m_currentHealth;
             _textCoton.text = $"nombre de coton : {_playerStats.m_currentHealth}" ;
         }
@@ -33,10 +31,9 @@ namespace Player.Runtime
                 Coton coton = other.GetComponent<Coton>();
                 if (coton is not null && !coton.CanBeCollected()) return;
                 
-                _playerStats.m_currentHealth++;
+                _playerStats.m_currentHealth += collectable.Collect();
                 m_coton = _playerStats.m_currentHealth;
                 _textCoton.text = $"nombre de coton : {_playerStats.m_currentHealth} " ;
-                collectable.Collect();
             }
         }
 
@@ -55,28 +52,11 @@ namespace Player.Runtime
         #endregion
         
         
-        #region Main Methods
-
-        // private void BuffStat()
-        // {
-        //     m_buff = m_coton / _numberCotonForBuff;
-        //     _playerStats.m_publicDamage=_playerStats.m_privateDamage+m_buff;
-        //     _playerStats.m_publicHP=_playerStats.m_privateHP+m_buff;
-        //     if (_playerMovement.m_speed > _minimalSpeed)
-        //     {
-        //         _playerMovement.m_speed = _playerMovement.m_speedSave - m_buff;
-        //     }
-        // }
-        
-        #endregion
-        
-        
         #region Private And Protected
 
         [SerializeField] private int _numberCotonForBuff = 5;
         [SerializeField] private TMP_Text _textCoton;
         private PlayerStats _playerStats;
-        private PlayerMovement _playerMovement;
         [SerializeField] private float _minimalSpeed=5;
 
         #endregion
